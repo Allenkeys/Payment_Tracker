7abc14f5d441f676ffe5c00f6211734d1e5eae4b
Joshua Eze <ezejoshuac@gmail.com>
2022-11-25 08:28:29 +0100

    Add project files.

---
+++ Payment_Tracker/Program.cs
@@ -0,0 +1,10 @@
+﻿ namespace Payment_Tracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Payment_Tracker/Tracker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment_Tracker
+{
+    internal class Tracker
+    {
+        public static int check(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
